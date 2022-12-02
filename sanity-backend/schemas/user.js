7fdeb608e5e9d0ca05d5521cc3c20456f9f13b86
sadegh <akbarisadegh@gmail.com>
2022-12-02 23:47:32 +0330

    create sanity bakcend

---
+++ sanity-backend/schemas/user.js
@@ -0,0 +1,16 @@
+export default {
+     name: 'user',
+     title: 'User',
+     type: 'document',
+     fields: [
+          {
+               name: 'nameName',
+               title: 'User Name',
+               type: 'string',
+          },{
+               name: 'image',
+               title: 'Image',
+               type: 'string',
+          }
+     ]
+}
