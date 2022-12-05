b726f0460ffa44a5d4eda843d2ca676b93be1dbb
sadegh <akbarisadegh@gmail.com>
2022-12-05 12:14:26 +0330

    change lock

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { footerList1, footerList2, footerList3 } from '../utils/constants'
 
-const List = ({ items }: { items: string[] }) => {
-     return (
-          items.map((item, i) => (
-               <p key={i} className='text-sm cursor-pointer text-gray-500 hover:underline'>{item}</p>
-          ))
-     )
-}
-
 const Footer = () => {
-  return (
-    <div className='hidden xl:block'>
-          <div className='flex flex-wrap gap-2'>
-               <List items={footerList1}/>
-               <List items={footerList2}/>
-               <List items={footerList3}/>
+     return (
+          <div className='hidden xl:block'>
+               <div className='flex flex-wrap gap-2'>
+                    {footerList1.map((item, i) => (
+                         <p key={i} className='text-sm cursor-pointer text-gray-500 hover:underline'>{item}</p>
+                    ))}
+               </div>
+               <div className='flex flex-wrap gap-2'>
+                    {footerList2.map((item, i) => (
+                         <p key={i} className='text-sm cursor-pointer text-gray-500 hover:underline'>{item}</p>
+                    ))}
+               </div>
+               <div className='flex flex-wrap gap-2'>
+                    {footerList3.map((item, i) => (
+                         <p key={i} className='text-sm cursor-pointer text-gray-500 hover:underline'>{item}</p>
+                    ))}
+               </div>
           </div>
-    </div>
-  )
+     )
 }
 
 export default Footer
