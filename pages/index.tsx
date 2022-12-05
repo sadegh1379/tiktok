435c71d73cd4057f3473573b0f1cd839b19e3459
sadegh <akbarisadegh@gmail.com>
2022-12-05 15:40:50 +0330

    connect to sanity backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
 
-export default function Home() {
+import axios from 'axios';
+import { client } from '../utils/client';
+import { allPostsQuery } from '../utils/queries';
+
+type VProp = {
+  videos: Video
+}
+
+export default function Home({ videos }: VProp) {
+  console.log('videos:', videos);
   return (
     <div>
       <p className="text-3xl font-bold underline">sadegh</p>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get('http://localhost:3000/api/post');
+  return{
+    props:{
+      videos: data
+    }
+  }
 }
